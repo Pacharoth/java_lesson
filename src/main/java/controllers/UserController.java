26298316b8c668d7940e998d489f40b4135d0cfb
unknown <pacharothroeun01@gmail.com>
2023-11-19 17:43:24 +0700

    [:fire:] add user

---
+++ src/main/java/controllers/UserController.java
@@ -0,0 +1,7 @@
+package controllers;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class UserController extends HttpServlet {
+    //TODO: compare bcrypt password
+}
---
+++ src/main/java/models/user/User.java
@@ -0,0 +1,5 @@
+package models.user;
+
+public class User {
+    //TODO: user properties (email,username,password)
+}
